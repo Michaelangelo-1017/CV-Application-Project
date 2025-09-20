4396877a2661a5289f18e8bff38b5a25bc981e11
Michaelangelo <jagbodyke01@gmail.com>
2025-09-20 20:14:34 +0100

    Update app.css,app.jsx and index.css for structure
    This commit will update the app.css, app.jsx and
    index.css files to display a start to the CV
    application app which has some limited elements
    and functionality for the mean time like toggling
    between adding general information and editing it.

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,61 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [fullName,setFullName] = useState('');
+  const [email,setEmail] = useState('');
+  const [phoneNumber,setPhoneNumber] = useState('');
+  const [isSubmitted,setIsSubmitted] = useState(false);
+  function handleSubmit(e){
+    e.preventDefault();
+    setIsSubmitted(!isSubmitted);
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='main-container'>
+      <div className='cv-details'>
+        <h1>CV Details Section</h1>
+        <div className="general-info">
+          <h2 className='title'>General Information</h2>
+          <p className={isSubmitted ? 'disabled' : ''}>Fill out the form!</p>
+          <form className={isSubmitted ? 'general-form disabled' : 'general-form'}>
+            <label htmlFor='full-name'>Enter Full Name (required)</label>
+            <input type="text" id='full-name' placeholder='e.g John Snow' required onChange={(e)=>setFullName(e.target.value)} />
+            <label htmlFor='email'>Enter Email (required)</label>
+            <input type="email" id='email' placeholder='e.g example@gmail.com' required onChange={(e)=>setEmail(e.target.value)} />
+            <label htmlFor='phone-no'>Enter Phone Number (required)</label>
+            <input type="number" id='phone-no' placeholder='e.g 07045687911' required onChange={(e)=>setPhoneNumber(e.target.value)} />
+            <button type='submit' id='submit-form' onClick={handleSubmit}>Submit Form</button>
+          </form>
+          <button className={!isSubmitted ? 'disabled' : ''} onClick={handleSubmit}>Edit General Info</button>
+        </div>
+        <div className="educational-experience">
+          <h2 className="title">Educational Experience</h2>
+          <div className='educational-info disabled'>
+            
+          </div>
+          <button>+ Add Education</button>
+        </div>
+        <div className="practical-experience">
+          <h2 className="title">Practical Experience</h2>
+          <button>+ Add Experience</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='cv-preview'>
+        <h1>CV Preview Section</h1>
+        <div>
+          <h2>General Information</h2>
+          <div>
+            <p>Full Name: {fullName}</p>
+            <p>Email: {email}</p>
+            <p>Phone Number: {phoneNumber}</p>
+          </div>
+          <h2>Educational Experience</h2>
+          <div></div>
+          <h2>Practical Experience</h2>
+          <div></div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
